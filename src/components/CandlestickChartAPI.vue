4f0594952639d60cb7ed879a84cf2e4326dd0a8c
programmarshakil <shakljoka103@gmail.com>
2024-11-26 13:49:48 -0800

    test

--- src/components/CandlestickChartAPI.vue
+++ src/components/CandlestickChartAPI.vue
@@ -42,7 +42,7 @@
                     d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                 </svg>
                 <!--
-            Icon when menu is open.
+            Icon when menu is open. 
 
             Menu open: "block", Menu closed: "hidden"
           -->
